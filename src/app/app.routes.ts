8a992c5f05b0d674c1bc381a3823bfb50552b676
Eric <eric.detommaso@gmail.com>
2025-04-16 20:48:22 +0200

    feat: add header and footer components, implement routing for home and 404 pages
    - Created HeaderComponent and FooterComponent with basic templates and styles.
    - Updated app.component.ts to include HeaderComponent and FooterComponent.
    - Implemented routing in app.routes.ts for home page and 404 page.
    - Added HomePageComponent and NotFoundPageComponent with basic templates.
    - Created NavbarComponent for navigation links.
    - Updated index.html to use a custom logo and changed language to French.
    - Modified global styles in styles.css for better appearance.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { 
+        path: "", component: HomePageComponent
+    },
+    {
+        path: "404", component: NotFoundPageComponent
+    },
+    {
+        path: "**",  redirectTo: "404"
+    }
+];
